f12e6852ed347f4fabe3befa8ab1bc6ff6e23fac
mariugul <marius.gulbrandsen@remarkable.no>
2024-08-17 00:57:39 +0200

    feat: minimal working example
    Comeit runs without any actual rule logic.
    The infrastructure can read rule configs from a yaml
    file, parse it into a Rule object that has all the information,
    call on the specific methods to run check on rules and
    print out if the rules passed or not in a nice format.
    
    It does not yet support dependencies.
    Does not yet support user configurations.
    Expects all rules to be ERROR's.
    Does not take any input arguments.

---
+++ comeit/checks/body.py
@@ -0,0 +1,10 @@
+# Body tests
+class Body():
+    def check_body(self):
+        """A full test of the body"""
+
+    def summary_ended_with_newline():
+        """Summary needs to end with newline for body to make sense to run
+        
+        
+        """
